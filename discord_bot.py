e374de1bd13d76a574af12470e444376aa7576b1
Philip Nguyen <ballot.stuffer@gmail.com>
2019-01-13 11:57:31 -0800

    Removed TOKEN var
    Replaced the TOKEN var holding the Discord bot auth key with a direct call to the env var that holds the value.

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,6 @@
 import discord
 import os
 
-TOKEN = os.environ.get('DISCORD_BOT_KEY') # NO HARDCODED TOKENS, LILY.
 client = discord.Client()
 tohangup = ""
 
@@ -28,7 +27,11 @@ async def on_message(message):
 
     elif message.content.startswith('!test-new'): # New code using rewrite branch method
             with message.channel.typing():
-                await message.channel.send('Message written using message.channel.send')   
+                await message.channel.send('Message written using message.channel.send')
+
+    elif message.content.startswith('!lightsoff'): # LIFX Test Implementation
+            with message.channel.typing():
+                   
 
     elif message.content.startswith('!add'):
         member = message.mentions[0]
@@ -104,4 +107,4 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.run(TOKEN)
+client.run(os.environ.get('DISCORD_BOT_KEY')) # NO HARDCODED TOKENS, LILY.
