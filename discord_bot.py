59f352a291fd35f8325106652823c4646187001b
Philip Nguyen <ballot.stuffer@gmail.com>
2019-02-02 22:59:27 -0800

    Cleaning up code, consistent ifs

--- discord_bot.py
+++ discord_bot.py
@@ -43,36 +43,23 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!nightnight'):
-    #    member = message.mentions[0]
-
-        for currentchannel in message.server.channels:
-            if(currentchannel.name == "Admiral"):
-              chanelid = currentchannel.id
-              channeltoremove = currentchannel
-        await client.delete_channel(channeltoremove)
-        await client.create_channel(message.server, 'Admiral', type=discord.ChannelType.voice)
-        msg = ('Good night. All users have been removed from Admiral.').format(message)
-        msg = msg.format(message)
-        await client.send_message(message.channel, msg)
-
-    elif message.content.startswith('!test-new'): # Test code using rewrite branch method
+    if message.content.startswith('!test-new'): # Test code using rewrite branch method
             with message.channel.typing():
                 await message.channel.send('Message written using message.channel.send')
 
-    elif message.content.startswith('!phil-lightstoggle'): # Toggle Phil's lights
+    if message.content.startswith('!phil-lightstoggle'): # Toggle Phil's lights
             await message.channel.send('Sending lights toggle command to LIFX bulbs @ Phil (all)')
             token = os.environ.get('LIFX_Key_Phil')
             response = requests.post('https://api.lifx.com/v1/lights/all/toggle', auth=(token, ''))
             await message.channel.send(response.text)
 
-    elif message.content.startswith('!phil-getlights'):
+    if message.content.startswith('!phil-getlights'):
             await message.channel.send('Getting lights status')
             token = os.environ.get('LIFX_Key_Phil')
             response = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
             await message.channel.send(response.text)
 
-    elif message.content.startswith('!phil-programmingmode'):
+    if message.content.startswith('!phil-programmingmode'):
             with message.channel.typing():
                 await message.channel.send("Changing lights to try and stay the fuck awake.")
                 token = os.environ.get('LIFX_Key_Phil')
@@ -84,7 +71,7 @@ async def on_message(message):
                 response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, auth=(token, ''))
                 await message.channel.send(response.text)
 
-    elif message.content.startswith('!phil-lightsoff'):
+    if message.content.startswith('!phil-lightsoff'):
         with message.channel.typing():
                 await message.channel.send("Night-night time.")
                 token = os.environ.get('LIFX_Key_Phil')
@@ -94,7 +81,7 @@ async def on_message(message):
                 response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, auth=(token, ''))
                 await message.channel.send(response.text)
             
-    elif message.content.startswith('!fuckyou-phil'): # Send a meanie text message to Phil
+    if message.content.startswith('!fuckyou-phil'): # Send a meanie text message to Phil
             await message.channel.send('Sending a fuck you to Phil')
             headers = {'x-profile-secret': os.environ.get('Telnyx_SMS_Key'), 'Content-Type':'application/json'}
             payload = json.dumps({'from': os.environ.get('Telnyx_Phone'), 'to': os.environ.get('Phils-cell'), 'body': 'Fuck you!'})
@@ -106,7 +93,7 @@ async def on_message(message):
             response = requests.post('https://sms.telnyx.com/messages', data=payload, headers=headers)
             await message.channel.send(response.text)
 
-    elif message.content.startswith('!fuckyou-lily'): # Send a meanie text message to Lily
+    if message.content.startswith('!fuckyou-lily'): # Send a meanie text message to Lily
             await message.channel.send('Sending a fuck you to Lily')
             headers = {'x-profile-secret': os.environ.get('Telnyx_SMS_Key'), 'Content-Type':'application/json'}
             payload = json.dumps({'from': os.environ.get('Telnyx_Phone'), 'to': os.environ.get('Lilys-cell'), 'body': 'Fuck you!'})
@@ -117,72 +104,5 @@ async def on_message(message):
                 print (headers)
             response = requests.post('https://sms.telnyx.com/messages', data=payload, headers=headers)
             await message.channel.send(response.text)
-                                       
-    elif message.content.startswith('!add'):
-        member = message.mentions[0]
-        msg = ('Adding %s to the priority' % member)
-        msg = msg.format(message)
-    #    role = message.guild.roles.find(r => r.name === "Moonman")
-        server = message.server
-        role = discord.utils.get(server.roles, name="Moonman")
-        await client.add_roles(member, role)
-    #    membr = message.mentions[0]
-    #    membr.addRole("Moonman").catch(console.error)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('!remove'):
-        member = message.mentions[0]
-        msg = ('Removing %s from the priority' % member)
-        msg = msg.format(message)
-    #    role = message.guild.roles.find(r => r.name === "Moonman")
-        server = message.server
-        role = discord.utils.get(server.roles, name="Moonman")
-        await client.remove_roles(member, role)
-    #    membr = message.mentions[0]
-    #    membr.addRole("Moonman").catch(console.error)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('!bot'):
-        msg = 'I am not a robot.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('Hello'):
-        msg = 'Helllllo.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('Hey'):
-        msg = 'Yo.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('hey'):
-        msg = 'sup dude.'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('hangup'):
-        msg = str(message.mentions[0])
-        await client.send_message(message.channel, msg)
-    #    msg = str(message.author)
-    #    await client.send_message(message.channel, msg)
-        server = message.server
-#        vClient = client.voice_client_in(server)
-#        chosenMember = server.get_member_named(msg)
-#        vClient.disconnect();
-        client.join_voice_channel('Admiral')
-        hah = client.is_voice_connected(server)
-        print(hah)
-    #    if message.mentions[0] == :
-    #        msg = 'Deleting {0.author.mention}'.format(message)
-    #        await client.send_message(message.channel, msg)
-#        tohangup = str(message.mentions[0])
-#        msg = str(client.voice_clients)
-#        await client.send_message(message.channel, msg)
-#        for x in client.voice_clients:
-#            msg = str(x.user.id)
-#            await client.send_message(message.channel, msg)
-#            if x.server == ctx.message.server:
-#                msg = str(x.user)
-#                await client.send_message(message.channel, msg)
-#                if x.user == tohangup:
-                #    msg = "Disconecting?".format(message)
-                #    await client.send_message(message.channel, msg)
-#                    return await x.disconnect()
-#            return await client.say("NO")
 
 client.run(os.environ.get('DISCORD_BOT_KEY')) # NO HARDCODED TOKENS, LILY.
